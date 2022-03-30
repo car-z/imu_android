20649cac83ae611c93881fa9ccf57d56b9d0b345
justinklchan <chan.justin.hk@gmail.com>
2022-03-30 01:56:31 -0700

    a

--- app/src/main/java/com/example/imu/MainActivity.java
+++ app/src/main/java/com/example/imu/MainActivity.java
@@ -49,9 +49,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         av = this;
 
+        // get permissions
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
         onRequestPermissionsResult(1,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},grantResults);
 
+        // ui logic
         Constants.startButton = (Button)findViewById(R.id.button);
         Constants.stopButton = (Button)findViewById(R.id.button2);
         lineChart = (LineChart)findViewById(R.id.linechart);
@@ -60,12 +62,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Constants.startButton.setEnabled(true);
         Constants.stopButton.setEnabled(false);
 
+        // defining sensors
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         linearAcclerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_FASTEST);
         sensorManager.registerListener(this, linearAcclerationSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
+        // on click listeners
         Constants.startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -103,6 +107,57 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (Constants.start) {
+            if (sensorEvent.sensor.equals(linearAcclerationSensor)) {
+                Constants.accx.add(sensorEvent.values[0]);
+                Constants.accy.add(sensorEvent.values[1]);
+                Constants.accz.add(sensorEvent.values[2]);
+
+                //graphing logic
+                graphData(sensorEvent.values);
+            }
+            else {
+                Constants.gravx.add(sensorEvent.values[0]);
+                Constants.gravy.add(sensorEvent.values[1]);
+                Constants.gravz.add(sensorEvent.values[2]);
+            }
+            Log.e("log",String.format("%s %.2f %.2f %.2f",sensorEvent.sensor.getName(),sensorEvent.values[0],sensorEvent.values[1],sensorEvent.values[2]));
+        }
+    }
+
+    public void graphData(float[] values) {
+        lineDataX.add(new Entry(counter,values[0]));
+        lineDataY.add(new Entry(counter,values[1]));
+        lineDataZ.add(new Entry(counter,values[2]));
+        if (lineDataX.size()>lim) {
+            lineDataX.remove(0);
+            lineDataY.remove(0);
+            lineDataZ.remove(0);
+        }
+        counter+=1;
+
+        LineDataSet data1 = new LineDataSet(lineDataX, "x");
+        LineDataSet data2 = new LineDataSet(lineDataY, "y");
+        LineDataSet data3 = new LineDataSet(lineDataZ, "z");
+        data1.setDrawCircles(false);
+        data2.setDrawCircles(false);
+        data3.setDrawCircles(false);
+        data1.setColor(((MainActivity)this).getResources().getColor(R.color.red));
+        data2.setColor(((MainActivity)this).getResources().getColor(R.color.green));
+        data3.setColor(((MainActivity)this).getResources().getColor(R.color.blue));
+        List<ILineDataSet> data = new ArrayList<>();
+        data.add(data1);
+        data.add(data2);
+        data.add(data3);
+
+        LineData lineData = new LineData(data);
+        lineChart.setData(lineData);
+        lineChart.notifyDataSetChanged();
+        lineChart.invalidate();
+    }
+
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_FASTEST);
@@ -112,51 +167,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if (Constants.start) {
-            if (sensorEvent.sensor.equals(linearAcclerationSensor)) {
-                Constants.accx.add(sensorEvent.values[0]);
-                Constants.accy.add(sensorEvent.values[1]);
-                Constants.accz.add(sensorEvent.values[2]);
-
-                lineDataX.add(new Entry(counter,sensorEvent.values[0]));
-                lineDataY.add(new Entry(counter,sensorEvent.values[1]));
-                lineDataZ.add(new Entry(counter,sensorEvent.values[2]));
-                if (lineDataX.size()>lim) {
-                    lineDataX.remove(0);
-                    lineDataY.remove(0);
-                    lineDataZ.remove(0);
-                }
-                counter+=1;
-                LineDataSet data1 = new LineDataSet(lineDataX, "x");
-                LineDataSet data2 = new LineDataSet(lineDataY, "y");
-                LineDataSet data3 = new LineDataSet(lineDataZ, "z");
-                data1.setDrawCircles(false);
-                data2.setDrawCircles(false);
-                data3.setDrawCircles(false);
-                data1.setColor(((MainActivity)this).getResources().getColor(R.color.red));
-                data2.setColor(((MainActivity)this).getResources().getColor(R.color.green));
-                data3.setColor(((MainActivity)this).getResources().getColor(R.color.blue));
-                List<ILineDataSet> data = new ArrayList<>();
-                data.add(data1);
-                data.add(data2);
-                data.add(data3);
-
-                LineData lineData = new LineData(data);
-                lineChart.setData(lineData);
-                lineChart.notifyDataSetChanged();
-                lineChart.invalidate();
-            }
-            else {
-                Constants.gravx.add(sensorEvent.values[0]);
-                Constants.gravy.add(sensorEvent.values[1]);
-                Constants.gravz.add(sensorEvent.values[2]);
-            }
-            Log.e("log",String.format("%s %.2f %.2f %.2f",sensorEvent.sensor.getName(),sensorEvent.values[0],sensorEvent.values[1],sensorEvent.values[2]));
-        }
     }
 
     @Override
